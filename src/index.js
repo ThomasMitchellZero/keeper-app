21aa81e95b9dbb5645a35c2c073a9f43b49cdd5d
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2021-12-18 16:57:52 -0500

    adding in app.js file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App'
 
 
 ReactDOM.render(
 
-  <h1>Hello React</h1>,
+  <App />,
   document.getElementById('root')
 );
 
@@ -12,10 +13,10 @@ ReactDOM.render(
 1. Create a new React app
 2. Create an app.jsx component
 3. create a Header.jsx component that renders a header element to show the Keeper App name in a <h1>
-
-
 4. Create a footer.jsx component taht renders a footer element to show a copyright with dynamically updated year.
 5. Create a Note.jsx component to show a <div> element with a <h1> for a title and a <p> for content.
+
+
 Make sure the app is styled like the following: https://w00gz.csb.app/
 
 Hint - you will need to study the classes in the styles.css file to apply styling.
