61f6c1a91b0560a35b94e9b7bed41686927a2f83
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2021-12-18 15:54:38 -0500

    adding in header, footer, and note item.

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+/*
+
+*/
+
+import React from "react";
+
+function Header(){
+    return(
+        <h1>Keeper App</h1>
+    );
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -7,3 +7,25 @@ ReactDOM.render(
   <h1>Hello React</h1>,
   document.getElementById('root')
 );
+
+/*
+1. Create a new React app
+2. Create an app.jsx component
+3. create a Header.jsx component that renders a header element to show the Keeper App name in a <h1>
+
+
+4. Create a footer.jsx component taht renders a footer element to show a copyright with dynamically updated year.
+5. Create a Note.jsx component to show a <div> element with a <h1> for a title and a <p> for content.
+Make sure the app is styled like the following: https://w00gz.csb.app/
+
+Hint - you will need to study the classes in the styles.css file to apply styling.
+
+
+
+
+
+
+
+
+
+*/
